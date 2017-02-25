201bbf78bde5ffe2e2ad8e4eb837c76df2430f6d
nicholasjativa <nicholasjativa@outlook.com>
2017-02-25 18:50:57 -0500

    finished all REST functions for items

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://localhost:27017/alexaslist')
 /* Create an Item (accessed at POST http://localhost:8080/api/items) */
 router.post('/', function(req, res) {
     var item = new Item();
+    item.id = req.body.id;
     item.name = req.body.name;
     item.categories = req.body.categories;
     item.description = req.body.description;
@@ -29,6 +30,40 @@ router.get('/', function(req, res) {
 
         res.json(items);
     });
-}); 
+});
+
+/* Get the Item with the id (accessed at GET http://localhost:8080/api/items/:id) */
+router.get('/:_id', function(req, res) {
+    Item.findOne({ id: req.params._id }, function(err, item) {
+        if (err) res.send(err);
+
+        res.json(item);
+    });
+});
+
+/* Update the Item with the id (accessed at PUT http://localhost:8080/api/items/:id) */
+router.put('/:_id', function(req, res) {
+    Item.findOne({ id: req.params._id }, function(err, item) {
+        if (err) res.send(err);
+
+        /* UPDATE LOGIC GOES HERE */
+
+        item.save(function(err) {
+            if (err) res.send(err);
+
+            res.json({ message: 'Item updated.' });
+        });
+    });
+});
+
+/* Delete the Item with the id (accessed at DELETE http://localhost:8080/api/items/id) */
+router.delete('/:_id', function(res, res) {
+    Item.remove({ id: req.params._id }, function(err, item) {
+        if (err) res.send(err);
+
+        res.json({ message: 'Successfully deleted.' });
+    });
+});
+
 
 module.exports = router;
--- models/item.js
+++ models/item.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ItemSchema = new Schema({
+    id: Number,
     name: String,
     categories: [String],
     description: String,
